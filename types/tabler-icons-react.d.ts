471adae132f282a9e36c28f055469b43040e4639
Alu-card19 <clintoncodes68@gmail.com>
2026-07-18 20:01:29 +0100

    fix: add TypeScript declarations for @tabler/icons-react
    - Create types/tabler-icons-react.d.ts declaration file
    - Resolves 'Could not find a declaration file' TypeScript error
    - Library doesn't provide native types, so we declare them ourselves
    - Includes all 18 icons used in the project

---
+++ types/tabler-icons-react.d.ts
@@ -0,0 +1,30 @@
+declare module '@tabler/icons-react' {
+  import React from 'react';
+
+  export interface IconProps {
+    size?: number;
+    stroke?: number;
+    color?: string;
+    className?: string;
+    style?: React.CSSProperties;
+  }
+
+  export const IconSeedling: React.ComponentType<IconProps>;
+  export const IconMedal: React.ComponentType<IconProps>;
+  export const IconTelescope: React.ComponentType<IconProps>;
+  export const IconCompass: React.ComponentType<IconProps>;
+  export const IconStack2: React.ComponentType<IconProps>;
+  export const IconBulb: React.ComponentType<IconProps>;
+  export const IconPuzzle: React.ComponentType<IconProps>;
+  export const IconStar: React.ComponentType<IconProps>;
+  export const IconChartBar: React.ComponentType<IconProps>;
+  export const IconSum: React.ComponentType<IconProps>;
+  export const IconTrophy: React.ComponentType<IconProps>;
+  export const IconSchool: React.ComponentType<IconProps>;
+  export const IconCrown: React.ComponentType<IconProps>;
+  export const IconAward: React.ComponentType<IconProps>;
+  export const IconCheck: React.ComponentType<IconProps>;
+  export const IconDownload: React.ComponentType<IconProps>;
+  export const IconClipboardList: React.ComponentType<IconProps>;
+  export const IconHelp: React.ComponentType<IconProps>;
+}
